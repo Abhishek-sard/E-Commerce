6175e635efec3bd828c75cf3bc1c15b71de2fa47
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-06-29 15:34:23 +0545

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
-import Navbar from '../src/Pages/Navbar.jsx'
-import Body from '../src/Pages/Body.jsx'
-import Demand from '../src/Pages/Demand.jsx'
+import Navbar from '../src/Pages/Navbar.jsx';
+import Body from '../src/Pages/Body.jsx';
+import Demand from '../src/Pages/Demand.jsx';
 import Products from './Pages/Products.jsx';
+import Customer from './Pages/Customer.jsx';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
       <Body />
       <Demand />
       <Products />
+      <Customer />
     </div>
   );
 }
---
+++ src/Pages/Customer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Customer = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-6 text-center">Our Customers</h2>
+      <p className="text-center text-gray-600">
+        We value our customers and strive to provide the best service possible.
+      </p>
+    </div>
+  )
+}
+
+export default Customer
