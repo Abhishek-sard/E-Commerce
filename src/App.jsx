1de645de8a3c888830f1ab385a5a38949a7e2441
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-06-29 14:46:17 +0545

    push

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Navbar from '../src/Pages/Navbar.jsx'
 import Body from '../src/Pages/Body.jsx'
 import Demand from '../src/Pages/Demand.jsx'
+import Products from './Pages/Products.jsx';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Body />
       <Demand />
+      <Products />
     </div>
   );
 }
---
+++ src/Pages/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div className="bg-gray-100 py-10 px-4">
+        <div className="flex items-center justify-center mb-8">
+            <h1 className='text-4xl *1 font-extrabold text-center'>OUR PRODUCTS</h1>
+        </div>
+    </div>
+
+  )
+}
+
+export default Products
